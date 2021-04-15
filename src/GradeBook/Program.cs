d300156f6c46652487e12411c557c74c784ca8cb
Rafa <rsg_2008@hotmail.es>
2021-04-14 22:38:20 -0600

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Rafa"); 
+            book.AddGrade(80.39);
+            book.AddGrade(35.45);
+            book.AddGrade(83.73);
+            book.AddGrade(38.82);
+            book.AddGrade(28.10);
+            book.AddGrade(19.20);
+
+            var stats = book.GetStatistics(); 
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The higest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+        }
+    }
+}
